5c29dfaa330d69fac3f6f6c8875826cb0648d34e
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-05-24 16:16:16 +0530

    frontend setup login seaseon and hero or navbar seassion only

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/',() => {
+  console.log("Server Started")
+})
+
+app.listen(PORT , () => {
+  console.log(`server start at port ${PORT}`)
+})

946aa9b4cbdbeadfe262cbf1ebc0e567202659e4
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-05-24 21:38:00 +0530

    add tailwind class for layout season to manage ui

--- frontend/src/pages/MainLayout.tsx
+++ frontend/src/pages/MainLayout.tsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen m-2 md:m-0">
+    <div className="flex flex-col min-h-screen">
       <header>
         <Navbar />
       </header>
-      <div className="">
+      <div className="flex-1">
         <Outlet />
       </div>
       <footer>

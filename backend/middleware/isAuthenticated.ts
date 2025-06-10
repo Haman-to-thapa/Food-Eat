c5e8fb976030cffb2de92490ac7f1665082c925a
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-06-10 11:18:31 +0530

    upadate new logic and chnages to manage

--- backend/middleware/isAuthenticated.ts
+++ backend/middleware/isAuthenticated.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response, Request } from "express";
 import jwt from 'jsonwebtoken'
 
+
 declare global {
   namespace Express {
     interface Request {
@@ -9,30 +10,34 @@ declare global {
   }
 }
 
-export const isAuthenticated = async (req: Request, res: Response, next : NextFunction) => {
+export const isAuthenticated = async (req: Request, res: Response, next : NextFunction) : Promise <void> => {
 try {
   const token = req.cookies.token;
 
   if(token) {
-    return res.status(401).json({
+     res.status(401).json({
       success:false,
       message:"User not authenticated"
     })
+    return
   }
 
-  const decode = jwt.verify(token, process.env.SECRET_KEY!) as jwt.jwtPayload;
+  const decode = jwt.verify(token, process.env.SECRET_KEY!) as jwt.JwtPayload;
 
   if(!decode) {
-    return res.status(401).json({
+    res.status(401).json({
       success:false,
       message:"Invalid token"
     })
+    return
   }
   req.id = decode.userId;
+
   next();
 } catch (error) {
-  return res.status(500).json({
+   res.status(500).json({
     message:"Middleware serer Error"
   })
+  return
 }
 }
